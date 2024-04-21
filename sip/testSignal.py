e5a5ae3070fea83310b94aa20294c7a58e84b151
Rainer Beth Andrew <100107630+rainerba@users.noreply.github.com>
2024-04-21 19:35:30 +0700

    main di laptop

--- sip/testSignal.py
+++ sip/testSignal.py
@@ -2,26 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 metode = "zeroC"
-f=330
-t=1
-
+f=330 # Frekuensi sinyal
+t=1 # Lama sinyal dalam detik
 harmonics = 6
 fs = 48000.
 
 if __name__ == '__main__':
-    samples = np.arange(t * fs) / fs
-    signal = np.sin(2 * np.pi * f * samples)
+    x = np.arange(t * fs) / fs
+    signal = np.sin(2 * np.pi * f * x)
+    # Harmoni
     for i in range(2,harmonics+1):
-        signal += np.sin(2 * np.pi * f * i * samples)/i
-    noise = np.random.normal(0, np.std(signal), len(signal))
-    x = np.float32(signal)
+        signal += np.sin(2 * np.pi * f * i * x)/i
+    # Amplitude berkurang
+    envelope = lambda x: np.exp(-x)
+    signal *= envelope(x)
+    # Dengan noise
+    # noise = np.random.normal(0, np.std(signal), len(signal))
+    # signal += noise
+    plt.plot(signal)
+    plt.show()
+    data = np.float32(signal)
+
     if metode == "yin":
         import Yin
-        frek = Yin.process_audio(x)
+        frek = Yin.process_audio(data)
     elif metode == "fft":
         import FFT
-        frek = FFT.estimate_frequency(x)
+        frek = FFT.estimate_frequency(data)
     elif metode == "zeroC":
         import Zero_Crossing
-        frek = Zero_Crossing.main(x)
+        frek = Zero_Crossing.main(data)
     print(frek)
