84447f03ba16c08e3d073d557f87ade621b612b9
rainerbetha <rainerbetha@gmail.com>
2024-05-08 18:29:01 +0700

    ganti algoritma zeroC

--- sip/Zero_Crossing.py
+++ sip/Zero_Crossing.py
@@ -9,79 +9,77 @@ def butter_bandpass(lowcut, highcut, fs, order):
     sos = butter(order, [low, high], analog=False, btype='band', output='sos')
     return sos
 
-def butter_bandpass_filter(data, lowcut=75., highcut=350., fs=48000., order=6):
+def butter_bandpass_filter(data, lowcut=75., highcut=350., fs=48000., order=3):
     sos = butter_bandpass(lowcut, highcut, fs, order=order)
     y = sosfilt(sos, data)
     return y
 
-# def main(x, senar):
-    # batas = [[293.66,369.99],[220.0,277.18],[174.61,220.],[130.81,164.81],[98.,123.47],[73.42,92.5]]
-    # x = butter_bandpass_filter(x, lowcut=batas[senar][0], highcut=batas[senar][1])
+def main(x, senar):
+    batas = [[293.66,369.99],[220.0,277.18],[174.61,220.],[130.81,164.81],[98.,123.47],[73.42,92.5]]
+    x = butter_bandpass_filter(x, lowcut=batas[senar][0], highcut=batas[senar][1])
 
-    # # Duplikasi data
-    # #x = np.append(x,x) # 16_384
-    # #x = np.append(x,x) # 32_768
-    # #x = np.append(x,x) # 65_536
+    # Duplikasi data
+    #x = np.append(x,x) # 16_384
+    #x = np.append(x,x) # 32_768
+    #x = np.append(x,x) # 65_536
     
-    # # Simple Moving Average
+    # Simple Moving Average
     # x = np.convolve(x,np.ones(20), 'same')
-    # abs = np.abs(x)
-    # max = np.max(abs)
 
-    # zcrP = 0
-    # zcrN = 0
-    # N = len(x)
-    # L = 1.2 * np.sum(abs) / N
-    # xP = x-L
-    # xN = x+L
-    # if max > 0.:
+    abs = np.abs(x)
+    max = np.max(abs)
+    zcrP = 0
+    zcrN = 0
+    N = len(x)
+    L = 1.2 * np.sum(abs) / N
+    xP = x-L
+    xN = x+L
+    
+    zeroCP = np.where(np.diff(np.sign(xP)))[0]
+    timeP = np.diff(zeroCP)
+    avgP = np.mean(timeP)
+    
+    zeroCN = np.where(np.diff(np.sign(xN)))[0]
+    timeN = np.diff(zeroCN)
+    avgN = np.mean(timeN)
+    
+    avg = (avgP + avgN) / 2
+    
+    if max > 0.001:
         # for n in range(N-1):
             # if xP[n] < 0 < xP[n+1]:
                 # zcrP+=1
             # if xN[n] < 0 < xN[n+1]:
                 # zcrN+=1
         # freq = (48000. * (zcrP + zcrN) / N) / 2.
-    # else:
-        # freq = 0.
-    # return freq, max
-    
-def main(x, senar):
-    max = np.max(np.abs(x))
-    batas = [[293.66,369.99],[220.0,277.18],[174.61,220.],[130.81,164.81],[98.,123.47],[73.42,92.5]]
-    x = butter_bandpass_filter(x, lowcut=batas[senar][0], highcut=batas[senar][1])
-    
-    zeroC = np.where(np.diff(np.sign(x)))[0]
-    time = np.diff(zeroC)
-    avg = np.mean(time)
-    if max > 0.01:
+        
         freq = 48000. / avg / 2.
     else:
         freq = 0.
     return freq, max
+    
+
 
 if __name__ == '__main__':
     import audio
-    import wave
-    import pyaudio
+    # import soundfile as sf
+    batas = [[293.66,369.99],[220.0,277.18],[174.61,220.],[130.81,164.81],[98.,123.47],[73.42,92.5]]
     maxx = []
     frames = []
     senar = int(input("Pilih senar yang akan diatur: ")) - 1
     while True:
         try:
             x = audio.ambil_data()
-            frames.append(x)
             frek,max = main(x,senar)
+            x = butter_bandpass_filter(x, lowcut=batas[senar][0], highcut=batas[senar][1])
+            if frek > 0:
+                frames = np.append(frames,x)
             print(frek)
             maxx.append(max)
         except KeyboardInterrupt:
+            # sf.write('zeroC.wav', np.ravel(frames), 48000)
+            # plt.plot(frames)
             plt.plot(maxx)
             plt.show()
             audio.stop()
-            wf = wave.open("zeroC.wav", 'wb')
-            wf.setnchannels(1)
-            wf.setsampwidth(pyaudio.PyAudio().get_sample_size(pyaudio.paFloat32))
-            wf.setframerate(48000)
-            wf.writeframes(b''.join(frames))
-            wf.close()
-            print("Program berhenti")
             break
--- sip/audio.py
+++ sip/audio.py
@@ -26,7 +26,7 @@ def ambil_data():
     y = stream.read(CHUNK, exception_on_overflow = False)
     data = np.frombuffer(y, dtype=np.float32)
     x = data.copy()
-    x -= np.mean(x)
+    # x -= np.mean(x)
     return x
 
 def stop():
@@ -36,7 +36,7 @@ def stop():
     print("Program Berhenti")
 
 def record():
-    import wave
+    import soundfile as sf
     RECORD_SECONDS = 5
     WAVE_OUTPUT_FILENAME = "voice.wav"
     print("* recording")
@@ -47,9 +47,7 @@ def record():
     print("*done recording")
     stop()
     
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(1)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(48000)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+    sf.write('zeroC.wav', np.ravel(frames), 48000)
+
+if __name__ == '__main__':
+    record()
